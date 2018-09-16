357f6df0910a4b7e9f0a14c807e3687bbc27339e
Leeson Chen <leesonchen@Leesons-MacBook-Pro.local>
2018-09-15 20:35:10 -0400

    First commit

---
+++ reversestring.py
@@ -0,0 +1,12 @@
+# This is a Python script to reverse a string.
+# Leeson Chen
+# September 15 2018
+
+words = raw_input("Enter the string to be reversed: ")
+for x in range(len(words)):
+	#print x
+	print words[len(words) - x - 1],
+	# adding a comma to the end stops a new line from printing
+	
+# holy shit python is so fucking easy to use
+# I love you smart snek
