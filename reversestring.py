81d024191c9db7180d506d91ed526d127bc49eca
Mayur Goyal <Mayurg6832.mg@gmail.com>
2020-10-01 13:51:58 +0530

    Created a new code
    This code uses string slicing to reverse a string

--- reversestring.py
+++ reversestring.py
@@ -2,7 +2,7 @@
 # Leeson Chen
 # September 15 2018
 
-words = raw_input("Enter the string to be reversed: ")
+words = input("Enter the string to be reversed: ")
 for x in range(len(words)):
 	#print x
 	print words[len(words) - x - 1],
@@ -10,3 +10,9 @@ for x in range(len(words)):
 	
 # jesus h christ python is so easy to use
 # I love you smart snek
+
+#Or a simpler one using string slicing
+
+words=words.split()
+words=words[::-1]
+print(*words)
